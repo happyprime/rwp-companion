dea3e96b9ab3e47428f2e83f8ec1894c59df89f1
Jeremy Felt <jeremy.felt@gmail.com>
2022-03-21 15:12:24 -0700

    Bump plugin version 1.1.0

--- plugin.php
+++ plugin.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Plugin Name:       RWP Companion
- * Plugin URI:        https://github.com/happyprime/rwp-companion/
+ * Plugin URI:        https://wordpress.org/plugins/rwp-companion/
  * Description:       A companion plugin for the RWP package used to publish to WordPress with R.
  * Author:            Happy Prime
  * Author URI:        https://happyprime.co
- * Version:           1.0.0
+ * Version:           1.1.0
  * Requires at least: 5.9
  * Requires PHP:      7.2
  */
